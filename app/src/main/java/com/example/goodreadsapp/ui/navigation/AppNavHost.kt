7d73b4c5cab268c63230492cdd901888a00db882
Polina Popova <irkomix2002@gmail.com>
2025-12-07 12:27:57 +0100

    refactor: move NavHost logic to AppNavHost.kt

--- app/src/main/java/com/example/goodreadsapp/ui/navigation/AppNavHost.kt
+++ app/src/main/java/com/example/goodreadsapp/ui/navigation/AppNavHost.kt
@@ -1,6 +1,7 @@
 package com.example.goodreadsapp.ui.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -15,10 +16,11 @@ import com.example.goodreadsapp.ui.screens.search.GenreScreen
 import com.example.goodreadsapp.ui.screens.search.SearchViewModel
 
 @Composable
-fun AppNavHost(navController: NavHostController) {
+fun AppNavHost(navController: NavHostController, modifier: Modifier = Modifier) {
     NavHost(
         navController = navController,
-        startDestination = NavRoutes.Home.route
+        startDestination = NavRoutes.Home.route,
+        modifier = modifier
     ) {
         val api = RetrofitClient.create("a1b147d54cmsh764647262b789e5p1b9179jsn9ad671dc4933")
         val repo = BooksRepository(api)
--- app/src/main/java/com/example/goodreadsapp/ui/navigation/GoodreadsApp.kt
+++ app/src/main/java/com/example/goodreadsapp/ui/navigation/GoodreadsApp.kt
@@ -25,31 +25,9 @@ fun GoodreadsApp() {
             BottomNavBar(navController)
         }
     ) { innerPadding ->
-        NavHost(
+        AppNavHost(
             navController = navController,
-            startDestination = NavRoutes.Home.route,
             modifier = Modifier.padding(innerPadding)
-        ) {
-            val api = RetrofitClient.create("a1b147d54cmsh764647262b789e5p1b9179jsn9ad671dc4933")
-            val repo = BooksRepository(api)
-            val homeVM = HomeViewModel(repo)
-            val searchVM = SearchViewModel(repo)
-
-            composable(NavRoutes.Home.route) { HomeScreen(viewModel = homeVM,
-                onBookClick = { id -> /* navigate later */ }) }
-            composable(NavRoutes.Search.route) { SearchScreen(viewModel= searchVM,
-                onBookClick= { id -> /* navigate later */ },
-                onGenreClick= { genre ->
-                    navController.navigate(NavRoutes.Genre.createRoute(genre)) }) }
-            composable(NavRoutes.Genre.route) { backStack ->
-                val genre = backStack.arguments?.getString("genre") ?: ""
-                GenreScreen(
-                    genre = genre,
-                    viewModel = searchVM,
-                    onBookClick = {}
-                )
-            }
-            composable(NavRoutes.MyBooks.route) { MyBooksScreen(navController) }
-        }
+        )
     }
 }
